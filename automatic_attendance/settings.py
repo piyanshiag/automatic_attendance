588d27a7fd45467921a9d1baf7a2246f25db35e6
piyanshi10 <piyanshi1091@gmail.com>
2022-05-28 19:23:28 +0530

    updated setting

--- automatic_attendance/settings.py
+++ automatic_attendance/settings.py
@@ -36,12 +36,12 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7j=@ua%qvih_!18nd^www3%8*xc6@@#ntvx_=(+fm(9!+n7(*+'
+SECRET_KEY = os.environ.get('DB_Secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smart-asistencia.herokuapp.com']
 
 
 # Application definition
